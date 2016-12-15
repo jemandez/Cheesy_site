0d013f759284ea82f546b9f0485cedbd0b145546
Miguel Angel Gordian <os.aioria@gmail.com>
2016-12-15 04:17:42 -0600

    Add the create method of collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -7,4 +7,14 @@ class CollectionsController < ApplicationController
     @collection = Collection.new
   end
 
+  def create
+    clean_params =  params[:collection].permit(:title, :description)
+    @collection = Collection.new clean_params
+
+    if @collection.save
+      redirect_to collections_path
+    else
+      render action: 'new'
+    end
+  end
 end
