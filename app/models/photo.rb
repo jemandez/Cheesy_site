1ca9bbab6404e7c3e232f3c7708d09b71618ae78
Miguel Angel Gordian <os.aioria@gmail.com>
2016-12-15 02:41:02 -0600

    Add url attribute and change validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,3 @@
 class Photo < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 2 }
-  validates :description, presence: true, length: { maximum: 400 }
+  validates :url, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213100050) do
+ActiveRecord::Schema.define(version: 20161215071353) do
 
   create_table "photos", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "url"
   end
 
 end
