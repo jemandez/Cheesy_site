a0b2cf0ef145d2cd427a6bf98a267e8965a84971
jemandez <jeman.400@gmail.com>
2017-03-04 00:08:58 -0600

    Added new route for schools

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get '/conoceme', to: "pages#conoceme"
   get '/contacto', to: "pages#contacto"
 
+  resources :schools, path: 'escuelas'
+
   root "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
