515971aec149b1492bf58ee2cd06696bd5d12cc2
jemandez <jeman.400@gmail.com>
2017-03-05 15:58:49 -0600

    New frontend components

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get '/contacto', to: "pages#contacto"
 
   resources :schools, path: 'escuelas'
-
+  resources :events, path: 'generaciones'
   root "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
