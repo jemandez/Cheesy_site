8c6f001b575c78f355107e830b31145ef822ffe0
Miguel Angel Gordian <os.aioria@gmail.com>
2017-03-11 18:36:37 -0600

    Add seed for schools and admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+School.create(name: "Benemérito Instituto Normal del Estado",
+              url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Logotipo_2014_BINE.png/220px-Logotipo_2014_BINE.png")
+School.create(name: "CENCH",
+              url: "http://bandasdemusicapuebla.mx/banda3/wp-content/uploads/2015/06/LOGO-BANDA-CENHCH.png")
+School.create(name: "Centro Escolar José Maria Morelos y Pavón",
+              url: "http://www.centroescolarmorelos.edu.mx/imagenes/cabeza_01.png")
+School.create(name: "Instituto Mexicano Madero",
+              url: "http://www.imm.edu.mx/toledo/images/easyblog_images/63/logoimm2.png")
+
+if Admin.count == 0
+  Admin.new({:email => "m@gmail.com", :password => "123456", :password_confirmation => "123456", :confirmed_at => Time.now }).save(false)
+end
