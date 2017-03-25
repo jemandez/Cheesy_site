a42ac9a2e1463a8abc3ca9f4611eb499e23bba86
Miguel Angel Gordian <os.aioria@gmail.com>
2017-03-25 03:20:07 -0600

    Groups only allows authenticated guests

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,9 +3,25 @@ class GroupsController < ApplicationController
   before_action :authenticate_admin!, except: [:index, :show]
 
   def index
-    @groups = Group.all
-    @generation = Generation.find(params[:generation_id])
+    school_id = params[:school_id]
+    generation_id = params[:id]
+
     @school = School.find(params[:school_id])
+    @generation = @school.generations.find(params[:generation_id])
+    @groups = @generation.groups
+
+
+    unless current_admin
+      if school_id != session[:school] or generation_id != session[:generation]
+        logger.info "Fail evaluation, no allowed resource"
+        render :status => 403
+      end
+
+      if session[:expires_at] < Time.current
+        flash[:notice] = "La sesión ha expirado, ¡vuelve a ingresar!"
+        redirect_to access_school_path(school_id)
+      end
+    end
   end
 
   def new
@@ -29,9 +45,25 @@ class GroupsController < ApplicationController
   end
 
   def show
+    school_id = params[:school_id]
+    generation_id = params[:id]
+
+
     @group = Group.find params[:id]
     @generation = Generation.find(params[:generation_id])
     @school = School.find(params[:school_id])
+
+    unless current_admin
+      if school_id != session[:school] or generation_id != session[:generation]
+        logger.info "Fail evaluation, no allowed resource"
+        render :status => 403
+      end
+
+      if session[:expires_at] < Time.current
+        flash[:notice] = "La sesión ha expirado, ¡vuelve a ingresar!"
+        redirect_to access_school_path(school_id)
+      end
+    end
   end
 
   def edit
