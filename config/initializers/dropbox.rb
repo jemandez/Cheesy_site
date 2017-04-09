68e6dd24cb00d2d282e906770be8911589574c4b
Miguel Angel Gordian <os.aioria@gmail.com>
2017-04-09 14:54:28 -0500

    The cheesy title is replaced by the cheesy logo

--- config/initializers/dropbox.rb
+++ config/initializers/dropbox.rb
@@ -1,4 +1,6 @@
 require 'dropbox'
 
-::DROPBOX_SECRET = ENV['DROPBOX_TOKEN']
-::DBX = Dropbox::Client.new(ENV['DROPBOX_ACCESS_TOKEN'])
+if ENV['DROPBOX_TOKEN']
+  ::DROPBOX_SECRET = ENV['DROPBOX_TOKEN']
+  ::DBX = Dropbox::Client.new(ENV['DROPBOX_ACCESS_TOKEN'])
+end
