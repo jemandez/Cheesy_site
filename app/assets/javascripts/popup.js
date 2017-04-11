e8e90893e3e337fd20516e9d820c2111c15eac14
Miguel Angel G <os.aioria@gmail.com>
2017-04-11 00:47:47 +0000

    Added next and previous images on popup

--- app/assets/javascripts/popup.js
+++ app/assets/javascripts/popup.js
@@ -1,34 +1,42 @@
 $(document).ready(function(){
   var modal = document.getElementById('myModal');
-  var img = document.getElementById('myImg');
   var modalImg = document.getElementById("img");
-
   var captionText = document.getElementById("modal-name");
   var captionface = document.getElementById("modal-face");
   var captionphone = document.getElementById("modal-phone");
   var captionmail = document.getElementById("modal-mail");
 
   $(".grid-image__container__picture").click(function(){
-      console.log("click!");
-      modal.style.display = "block";
-      modalImg.src = this.src;
-      captionText.innerHTML = this.name;
-      captionface.innerText = this.getAttribute("face");
-      captionphone.innerText = this.getAttribute("phone");
-      captionmail.innerText = this.getAttribute("mail");
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.name;
+    captionface.innerText = this.getAttribute("face");
+    captionphone.innerText = this.getAttribute("phone");
+    captionmail.innerText = this.getAttribute("mail");
+    $(this).parent().addClass("active");
   })
 
-  var span = document.getElementsByClassName("close")[0];
-
-  $(span).click(function(){
-    console.log("hola span!");
+  $(".close").click(function(){
     modal.style.display = "none";
+    $(".active").removeClass("active");
   })
+
+  $(".next-image").click(function(){
+    $(".active").next().addClass("active");
+    $(".active").prev().removeClass("active");
+    $(".active").children().click();
+  });
+
+  $(".prev-image").click(function(){
+    $(".active").prev().addClass("active");
+    $(".active").next().removeClass("active");
+    $(".active").children().click();
+  });
 
   $(window).click(function(){
-    console.log("hola window!");
     if (event.target == modal) {
-        modal.style.display = "block";
+        modal.style.display = "none";
     }
   })
 })
+
